51a6943a996c313b0be710d368ba26fa86b22470
Monique Jackson <bymoniquejackson@gmail.com>
2022-06-01 15:03:52 +0100

    another update

---
+++ src/movie/MovieControllers.js
@@ -0,0 +1,22 @@
+const { response } = require("express");
+const Movie = require ("./MovieModel")
+
+exports.addMovie = async(req, res) => {
+    try {
+        const newMovie = await Movie.create(req.body);
+        res.status(200).send({movie: newMovie})
+    }catch(error){
+        console.log(error);
+        res.status(500).send({error: error.messge})
+    }
+};
+
+exports.listMovies = async (req, res) => {
+    try {
+        const movies = await Movie.find({});
+        res.status(200).send({movies});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({error: error.message})
+    }
+};
